3e3f3eaa00a9f2860462eea4010df3f72516b269
nadinnadin <nadiia.diakova@nure.ua>
2019-10-29 11:50:29 +0200

    fix exception

--- Дз6/Уровень3/abs3lvl.php
+++ Дз6/Уровень3/abs3lvl.php
@@ -5,11 +5,21 @@ $array2 = ["jfkdlsjvb", "reoejghe", "eptibnvdkfi"]; // второй массив
 
 function absr($arr1, $arr2){ // обьявление функции
 	if(count($arr1) && count($arr2) < 3){ //проверка массива
-		throw new Exception("Wrong Massive"); // определение ошибки
+		throw new Exception("Wrong Array"); // определение 
+ошибки
 		
 	}
 	$res = []; //массив для записи результатов
 	for ($i = 0; $i < 3; $i++){ // цикл для прохождения по каждой строке массива
+	
+	if(!is_string($arr1[$i])){
+		throw new Exception("Elements of first array are 
+not strings");
+	}
+	if(!is_string($arr2[$i])){
+		throw new Exception("Elements of second array 
+are not strings");
+	}
 
 		$res[] = abs(strlen($arr1[$i]) - strlen($arr2[$i])); //нахождение разницы длин по модулю строк
 	}
